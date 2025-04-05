4ac15b5504075e2fcff7d4debcf047a6465455c9
Mark Devenyi <markdevenyidev@gmail.com>
2025-04-04 23:37:46 -0400

    feat(core): add `kritic_register()` and `kritic_run_all()`

--- kritic.h
+++ kritic.h
@@ -1,4 +1,25 @@
 #ifndef __KRITIC_HEADER_GUARD
 #define __KRITIC_HEADER_GUARD
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Structs and types */
+typedef struct {
+    const char* suite;
+    const char* name;
+    void (*fn)(void);
+} kritic_test_t;
+
+typedef void (*kritic_test_fn)(void);
+
+/* API */
+void kritic_register(const char* suite, const char* name, void (*fn)(void));
+int kritic_run_all(void);
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
 #endif // __KRITIC_HEADER_GUARD
--- src/kritic.c
+++ src/kritic.c
@@ -1,8 +1,51 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "../kritic.h"
 
+#define KRITIC_MAX_TESTS 1024
+
+static kritic_test_t kritic_tests[KRITIC_MAX_TESTS];
+static int kritic_test_count = 0;
+
+/* Register a test function to a specific suite with a specific name */
+void kritic_register(const char *suite, const char *name, kritic_test_fn fn) {
+    if (kritic_test_count >= KRITIC_MAX_TESTS) {
+        fprintf(stderr, "[kritic] Too many registered tests!\n");
+        exit(1);
+    }
+
+    kritic_tests[kritic_test_count++] = (kritic_test_t) {
+        .suite = suite,
+        .name = name,
+        .fn = fn
+    };
+}
+
+/* Run all of the test suites and tests */
+int kritic_run_all(void) {
+    int failed = 0;
+
+    if (kritic_test_count == 0) {
+        printf("[kritic] No registered test found\n");
+    } else {
+        printf("[kritic] Running %d tests:\n", kritic_test_count);
+    }
+
+    for (int i = 0; i < kritic_test_count; ++i) {
+        const kritic_test_t *t = &kritic_tests[i];
+
+        printf("-> %s.%s\n", t->suite, t->name);
+
+        t->fn();
+    }
+
+    printf("[kritic] Finished running %d tests\n", kritic_test_count);
+
+    return failed > 0 ? 1 : 0;
+}
+
 int main(void) {
     printf("Hello World!\n");
-    return 0;
+    return kritic_run_all();
 }
