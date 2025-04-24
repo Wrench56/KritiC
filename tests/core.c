52b170a10a2409c71680d767ab64af231843f96e
Mark Devenyi <markdevenyidev@gmail.com>
2025-04-24 12:05:55 -0400

    test: verify `main()` override with custom entry point

--- tests/core.c
+++ tests/core.c
@@ -1,3 +1,12 @@
+#include <stdio.h>
+
 #include "../kritic.h"
 
 KRITIC_TEST(core, registration) {}
+
+int main(void) {
+    kritic_enable_ansi();
+    printf("Running from custom main()...\n");
+
+    return kritic_run_all();
+}
